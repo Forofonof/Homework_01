b989ed6b02d454b0d065afbd01915ba886de6519
Forofonof <DanielResult1@outlook.com>
2022-04-01 18:09:32 +0300

    Добавьте файлы проекта.

---
+++ Homework_01/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        int priceItem;
+        uint quantityOfItems;
+        bool isPlayerHaveFlag;
+        byte playerSpeed;
+        sbyte playerDamage;
+        short time;
+        ushort playerLevel;
+        float enemiesQuantity;
+        byte ammoQuantity;
+        int distanse;
+    }
+}
